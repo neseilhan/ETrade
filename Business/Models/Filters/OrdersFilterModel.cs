b37c6ac50b47339f9f41f18e5f90e823355216b5
Neşe İLHAN <neseilhannn@gmail.com>
2022-03-09 00:33:19 +0300

    .

---
+++ Business/Models/Filters/OrdersFilterModel.cs
@@ -0,0 +1,43 @@
+﻿using Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Business.Models.Filters
+{
+    public class OrdersFilterModel
+    {
+        [DisplayName("Date")]
+        public string DateBeginText { get; set; }
+
+        public string DateEndText { get; set; }
+
+        [DisplayName("User")]
+        public string UserName { get; set; }
+
+        List<OrderStatusModel> _orderStatuses;
+        public List<OrderStatusModel> OrderStatuses
+        {
+            get
+            {
+                _orderStatuses = new List<OrderStatusModel>();
+                Array values = Enum.GetValues(typeof(OrderStatus));
+                for (int i = 0; i < values.Length; i++)
+                {
+                    _orderStatuses.Add(new OrderStatusModel()
+                    {
+                        Value = ((int)values.GetValue(i)).ToString(),
+                        Text = values.GetValue(i).ToString()
+                    });
+                }
+                return _orderStatuses;
+            }
+        }
+
+        [DisplayName("Status")]
+        public List<int> OrderStatusValues { get; set; }
+
+        [DisplayName("Order ID")]
+        public string OrderId { get; set; }
+    }
+}
---
+++ Business/Services/Bases/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using AppCore.Business.Models.Paging;
+using AppCore.Business.Models.Results;
+using AppCore.Business.Services.Bases;
+using Business.Models;
+using Business.Models.Filters;
+using Business.Models.Reports;
+using System.Collections.Generic;
+
+namespace Business.Services.Bases
+{
+    public interface IProductService : IService<ProductModel>
+    {
+        //[Obsolete("Bu methodun daha yeni bir versiyonu bulunmaktadır.")]
+        // obsolete: kullanıldığı yerde kullanıldığı yapının daha yeni bir versiyonu olduğunu ve bu yeni versiyonun kullanılmasının gerektiğini belirtir. 
+        Result<List<ProductsReportModel>> GetProductsReport(ProductsReportFilterModel filter = null, PageModel page = null, AppCore.Business.Models.Ordering.OrderModel order = null);
+    }
+}
