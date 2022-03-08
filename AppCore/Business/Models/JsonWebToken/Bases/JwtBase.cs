f50a337a2f4a4a73a125a337f172f10cc4631117
Neşe İLHAN <neseilhannn@gmail.com>
2022-03-08 23:53:24 +0300

    .

---
+++ AppCore/Business/Models/JsonWebToken/Bases/JwtBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AppCore.Business.Models.JsonWebToken.Bases
+{
+    public abstract class JwtBase
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
