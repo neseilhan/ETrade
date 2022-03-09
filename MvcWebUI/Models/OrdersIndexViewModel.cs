5be607148bcab5d6d7404646a37c1f11f95ca1fb
Neşe İLHAN <neseilhannn@gmail.com>
2022-03-09 10:44:37 +0300

    .

---
+++ MvcWebUI/Models/OrdersIndexViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Business.Models;
+using Business.Models.Filters;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcWebUI.Models
+{
+    public class OrdersIndexViewModel
+    {
+        public List<OrderModel> Orders { get; set; }
+        public OrdersFilterModel Filter { get; set; }
+
+        MultiSelectList _orderStatusMultiSelectList;
+        public MultiSelectList OrderStatusMultiSelectList
+        {
+            get
+            {
+                _orderStatusMultiSelectList = new MultiSelectList(Filter.OrderStatuses, "Value", "Text");
+                return _orderStatusMultiSelectList;
+            }
+        }
+    }
+}
